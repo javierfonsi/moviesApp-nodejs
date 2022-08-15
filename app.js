d7f11e36fe37fcd0a8c58963aa01e6c0ae5b8fc8
javierfonsi <javier_fonsi@hotmail.com>
2022-08-15 03:59:50 -0500

    Swagger server changes

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const swaggerSpec = {
             "description": "Development server"
           },
           {
-            "url": "https://moviesappbyjrfl.herokuapp.com/api/v1/doc",
+            "url": "https://moviesappbyjrfl.herokuapp.com/",
             "description": "Production server"
           },
       ]
