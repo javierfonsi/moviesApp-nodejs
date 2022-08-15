d7880b58f551ede0862d05e3956caa3be302f08a
javierfonsi <javier_fonsi@hotmail.com>
2022-08-15 04:18:29 -0500

    Swagger server changes database

--- utils/database.js
+++ utils/database.js
@@ -10,14 +10,13 @@ const sequelize = new Sequelize({
   port: 5432,
   database: process.env.DB,
   dialect: 'postgres',
-  logging: false
-  //,
-  //   dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false
-  //   }
-  // }
+  logging: false,
+     dialectOptions: {
+     ssl: {
+       require: true,
+       rejectUnauthorized: false
+     }
+   }
 });
 
 module.exports = { sequelize };
